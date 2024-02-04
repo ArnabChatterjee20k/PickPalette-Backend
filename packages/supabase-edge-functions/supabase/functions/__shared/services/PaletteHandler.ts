4f8a97c116af1b12817422b59ed5201a35369115
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-02-04 13:57:30 +0530

    bug -- fixed quering for non existent project

--- packages/supabase-edge-functions/supabase/functions/__shared/services/PaletteHandler.ts
+++ packages/supabase-edge-functions/supabase/functions/__shared/services/PaletteHandler.ts
@@ -3,26 +3,45 @@ import { supabaseClient } from "../supabaseClient.ts";
 import { Database } from "../types/supabase.ts";
 type PaletteTable = Database["public"]["Tables"]["palette"];
 import DefaultColors from "../constants/DefaultColors.ts";
+import ProjectHandler from "./ProjectHandler.ts";
 export class PaletteHandler {
   private async create(projectId: number) {
-    const { status, error } = await supabaseClient
+    const { data, status, error } = await supabaseClient
       .from("palette")
-      .insert({ colors: DefaultColors, project_id: projectId });
-    if (status === 201) return HTTPResponse("SUCCESS", status);
+      .insert({ colors: DefaultColors, project_id: projectId })
+      .select("colors");
+    if (status === 201) return HTTPResponse("SUCCESS", status, data);
     return HTTPResponse("ERROR", status);
   }
   public async get(project_id: PaletteTable["Row"]["project_id"]) {
     const { data, status, error } = await supabaseClient
-      .from("palette")
-      .select("colors")
-      .eq("project_id", project_id);
+      .from("project")
+      .select(
+        `
+      name,
+      id,
+      palette(colors)
+      `
+      )
+      .eq("id", project_id);
     if (error) return HTTPResponse("ERROR", status);
-    if (!data || data.length === 0) {
+
+    // If project exists but palette does not exists create the palettes
+    if (data[0]?.id && data[0].palette.length === 0) {
       const { code, status, data } = await this.create(project_id);
-      console.log({data})
       return HTTPResponse("SUCCESS", code, data);
     }
-    return HTTPResponse("SUCCESS", status, data);
+
+    // if project does not exists return error
+    if (!data || data.length === 0) return HTTPResponse("ERROR", 404);
+
+    // if both palette exists send the palette
+    const result = {
+      name: data[0].name,
+      id: data[0].id,
+      colors: data[0].palette[0].colors,
+    };
+    return HTTPResponse("SUCCESS", status, [result]);
   }
   public async update(projectId: number, colors: string[]) {
     const { data, status, error } = await supabaseClient
