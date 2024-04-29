b0cdc2823d7c11a7e588070e176a766da9679e78
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-04-30 01:56:56 +0530

    fix -- reduced the complexity of the product hunt crawler

--- packages/scraper/src/core.ts
+++ packages/scraper/src/core.ts
@@ -16,136 +16,83 @@ interface LoggerOption {
   fileName: string;
 }
 export class ProductHuntCrawler {
-  public browser: Browser;
-  public page: Page;
-  public count: number;
-  public generalDataLogger: Logger;
-  public linksLogger: Logger;
-
+  private browser: Browser;
+  private page: Page;
+  private count: number;
+  private generalDataLogger: Logger;
+  private linksLogger: Logger;
+  private screenshotPath: string;
   constructor(public config: Config) {
     this.count = 0;
-    this.generalDataLogger = getLogger("general");
-    this.linksLogger = getLogger("links");
+    this.screenshotPath = "img/test.png";
   }
   async init(): Promise<ProductHuntCrawler> {
-    this.browser = await pw.chromium.launch({ headless: true });
+    this.browser = await pw.chromium.launch({ headless: false });
     this.page = await this.browser.newPage();
     return this;
   }
 
   async crawl() {
-    const {
-      url,
-      include = [],
-      exclude = [],
-      maxQueries,
-      onVisit,
-    } = this.config;
+    /**
+     * getting product links from a page
+     * then start going to each product page. Here can be a chance of getting some overlay. So close it
+     * Collect description and then visit the product main page. Get title from here
+     * Screenshot it and save it to the file storage.
+     * Generate palette from the taken screenshot
+     */
+    const { url, maxQueries } = this.config;
     const visitedPages = new Set();
     const errorsInPages = [];
     try {
-      await this.page.goto(url, { waitUntil: "domcontentloaded" });
-
-      let links = await this.getAllLinks({
-        saveToLogger: false,
-        include,
-        exclude,
-      });
+      await this.navigate(url);
+      const links = await this.getAllLinks();
 
       while (this.count < maxQueries && links.length) {
         const link = links.shift();
         if (visitedPages.has(link)) continue;
         visitedPages.add(link);
-
-        // Check count before processing the link
-        if (this.count >= maxQueries) {
-          break;
-        }
-
-        try {
-          console.log("Going to ", link);
-          await this.page.goto(link, { waitUntil: "domcontentloaded" });
-        } catch (error) {
-          console.log(error);
-          break;
-        }
-
-        // Close overlay if present
-        try {
-          const closeButton = this.page
-            .locator(".overlay")
-            .locator('a[title="Close"]')
-            .first();
-          if (await closeButton.isVisible()) {
-            await closeButton.click({ timeout: 60000 }); // 60 seconds timeout
-          }
-        } catch (error) {
-          console.log(error);
-        }
-
-        try {
-          await this.page.waitForSelector(
-            "xpath=//html/body/div[1]/div[2]/div[3]/main/div/div/div[2]/div[2]"
-          );
-          const description = await this.page
-            .locator(
-              "xpath=//html/body/div[1]/div[2]/div[3]/main/div/div/div[2]/div[2]"
-            )
-            .innerText();
-
-          const visitWebsiteLink = await this.page.evaluate(() => {
-            const elements = Array.from(document.querySelectorAll("a")).filter(
-              (a) => a.innerText === "Visit website"
-            );
-            return elements.length ? elements[0].href : null;
-          });
-
-          try {
-            if (visitWebsiteLink) {
-              await this.page.goto(visitWebsiteLink, {
-                timeout: 2 * 60 * 1000,
-                waitUntil: "domcontentloaded",
-              });
-            }
-          } catch (error) {
-            console.error("Error navigating to the website:", error.message);
-          }
-          try {
-            const title = await this.page.title();
-            await this.takeScreenshot();
-            await this.savePaletteToDB(title, description);
-          } catch (error) {
-            console.log(error);
-          }
-        } catch (error) {
-          errorsInPages.push({
-            product: link,
-            error: error.message,
-          });
-        }
-
-        // Additional waiting strategy if needed
-        // await this.page.waitForTimeout(2000); // Wait for 2 seconds
+        const errorInCrawledPage = await this.crawlPage(link);
+        if (errorInCrawledPage) errorsInPages.push(errorInCrawledPage);
         console.log(this.count);
         this.count++;
       }
     } catch (error) {
-      console.error("Error during crawling:", error);
+      console.log({ error, url });
+      errorsInPages.push({url,error})
     } finally {
-      // Close the browser after crawling
-      if (this.browser) {
-        await this.browser.close();
-      }
+      this.browser.close();
+    }
 
-      if (errorsInPages.length > 1)
-        (await this.generalDataLogger).error({ errors: errorsInPages });
+    return errorsInPages
+  }
+
+  async crawlPage(link: string) {
+    try {
+      await this.navigate(link);
+      await this.closeOverlayFromProductPageIfPresent();
+      const productData = await this.getProductDetails();
+      if (productData) {
+        const { title, description } = productData;
+        await this.takeScreenshot();
+        const palette = await generatePalettes(this.screenshotPath);
+        savePaletteToCache({ title, description, palette });
+        return null;
+      }
+    } catch (error) {
+      console.log({ error, link });
+      return { error, link };
     }
   }
 
-  async getAllLinks(option: LinkOption) {
-    const { saveToLogger, exclude, include } = option;
-    const args = { include, exclude };
-    const links = await this.page.evaluate(({ include, exclude }) => {
+  async navigate(url: string, timeout?: number) {
+    console.log("visiting url ", url);
+    await this.page.goto(url, {
+      waitUntil: "domcontentloaded",
+      timeout: timeout || 0,
+    });
+  }
+  async getAllLinks() {
+    const links = await this.page.evaluate(() => {
       return Array.from(document.querySelectorAll("a"))
         ?.map((a) => a.href)
         ?.filter(
@@ -153,17 +100,44 @@ export class ProductHuntCrawler {
             link.startsWith("https://www.producthunt.com/products") ||
             link.startsWith("https://www.producthunt.com/posts")
         );
-    }, args);
-    const pageTitle = await this.page.title();
+    });
     const uniqueLinks = Array.from(new Set(links));
-    (await this.linksLogger).info({ links: uniqueLinks });
     return uniqueLinks;
   }
-
-  takeScreenshot() {
-    return this.page.screenshot({ path: "img/test.png", fullPage: true });
+  async closeOverlayFromProductPageIfPresent() {
+    const closeButton = this.page
+      .locator(".overlay")
+      .locator('a[title="Close"]')
+      .first();
+    if (await closeButton.isVisible()) {
+      await closeButton.click({ timeout: 60000 }); // 60 seconds timeout
+    }
   }
+  async getProductDetails() {
+    const descriptionTag = await this.page.waitForSelector(
+      "xpath=//html/body/div[1]/div[2]/div[3]/main/div/div/div[2]/div[2]"
+    );
+    if (!(await descriptionTag.isVisible())) return null;
+    const description = await descriptionTag.innerText();
+    const visitWebsiteLink = await this.getProductMainPageVisitingLink();
+    if (!visitWebsiteLink) return null;
+    await this.navigate(visitWebsiteLink, 2 * 60 * 1000);
+    const title = await this.page.title();
+    return { title, description };
+  }
+  async takeScreenshot() {
+    return this.page.screenshot({ path: this.screenshotPath, fullPage: true });
+  }
+  async fetchProductPalettesFromProductPageScreenshot() {}
 
+  async getProductMainPageVisitingLink() {
+    return await this.page.evaluate(() => {
+      const elements = Array.from(document.querySelectorAll("a")).filter(
+        (a) => a.innerText === "Visit website"
+      );
+      return elements.length ? elements[0].href : "";
+    });
+  }
   async savePaletteToDB(title: string, description: string) {
     const data = await generatePalettes("img/test.png");
     savePaletteToCache({ description, title, palette: data });
